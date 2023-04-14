534d79b5c4e51603ec70fa76c37e0bdc7c92b927
Artur.H.I <117451939+Weaving-Instinct@users.noreply.github.com>
2023-04-14 17:07:26 -0300

    Add files via upload

---
+++ maratona-2022.A.c
@@ -0,0 +1,49 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int  click, clock, contador=0;
+char strong[2], *string;
+
+int main ()
+{
+string = &strong[2];
+
+
+scanf ("%d\n", &clock);
+
+
+
+string = ((char*) malloc(sizeof(char) * clock));
+
+string = realloc(string, clock * sizeof(char));
+
+for (click=0; click<clock; click++) {
+
+scanf ("%c", &strong[click]);
+
+}
+
+
+for (click=0;click<clock;click++){
+
+if ((strong[click]=='a') &&  (strong[click+1]=='a')) {
+
+    while( strong[click]=='a'){
+contador++;
+click++;
+    }
+}
+
+}
+
+
+printf ("%d", contador);
+
+
+free(string);
+
+return 0;
+
+}
+
+//H.I.
